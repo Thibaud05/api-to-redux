4d5531ec728b6f4be2594d38b6c9dd538ac40fa2
Thibaud <t.granier@studio05.fr>
2018-04-12 11:06:49 +0200

    Update api.test.js

--- api.test.js
+++ api.test.js
@@ -3,10 +3,16 @@ import thunk from 'redux-thunk'
 import fetchMock from 'fetch-mock'
 
 import API from "./api"
+import LocalStorageMock from "./LocalStorageMock"
 
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
+global.localStorage = new LocalStorageMock
+
+
+
+const token = 'cK.Jf5L_KD6ddsd=s5'
 
 describe('TEST REST API CRUD', () => {
 
@@ -18,9 +24,8 @@ describe('TEST REST API CRUD', () => {
     })
 
     it('constructor', () => {
-        expect(testApi.resourceType).toBe('_COMPANIES');
         expect(testApi.useJWT).toBe(false);
-        expect(testApi.ressourceUrl).toBe('https://api.awesome.com/companies/');
+        expect(testApi.ressourceUrl).toBe('http://127.0.0.1:3333/companies/');
     });
 
     it('fetchResource()', () => {
@@ -97,21 +102,86 @@ describe('TEST REST API CRUD', () => {
 
             expect(store.getActions()).toEqual([{ type: 'REMOVE_COMPANIES', payload: 1  }])
         })
-        
-        it('nested()', () => {
+    })
 
-            const nestedApi = new API('leaders')
-            nestedApi.nested('companies',1)
+    it('nested()', () => {
 
-            expect(nestedApi.resourceType).toBe('_LEADERS');
-            expect(nestedApi.ressourceUrl).toBe('http://127.0.0.1:3333/companies/1/leaders/');
-        });
+        const nestedApi = new API('leaders')
+        nestedApi.nested('companies',1)
+        expect(nestedApi.ressourceUrl).toBe('http://127.0.0.1:3333/companies/1/leaders/');
+    });
 
-        it('auth()', () => {
+    it('auth()', () => {
 
-            const authApi = new API('leaders')
-            authApi.auth()
-            expect(authApi.useJWT).toBe(true);
-        });
+        const authApi = new API('leaders')
+        authApi.auth()
+        expect(authApi.useJWT).toBe(true);
+    });
+
+
+
+    it('login', () => {
+        const testAuth = new API('login')
+        const param = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: '{"email":"e@mail.com","password":"secret"}' }
+
+        fetchMock
+            .postOnce({
+                matcher: function(url, opts) {
+                    expect(opts).toEqual(param)
+                    return (url === testAuth.ressourceUrl);
+                },
+                response: {token:token}
+            })
+
+        const store = mockStore()
+
+        return store.dispatch(testAuth.login({email:"e@mail.com",password:"secret"})).then(() => {
+
+            const actions = [
+                { type: 'REQUEST_LOGIN', loading: true },
+                { type: 'RECEIVE_LOGIN', loading: false, data: { isLogged: true }},
+            ]
+            expect(store.getActions()).toEqual(actions)
+        })
+    })
+
+    const testApiWithAuth = new API('companies')
+    testApiWithAuth.auth()
+
+    it('fetchResource with auth', () => {
+        fetchMock
+            .getOnce(testApiWithAuth.ressourceUrl, { body: { companies: ['do something'] }, headers: { 'content-type': 'application/json' } })
+
+        const expectedActions = [
+            { type: 'REQUEST_COMPANIES', loading: true  },
+            { type: 'RECEIVE_COMPANIES', data: { companies: ['do something'] }, loading: false }
+        ]
+        const store = mockStore()
+
+        return store.dispatch(testApiWithAuth.fetchResource()).then(() => {
+
+            expect(store.getActions()).toEqual(expectedActions)
+        })
+    })
+
+    it('addResource with auth', () => {
+        const param = { method: 'POST', headers: { 'Content-Type': 'application/json',Authorization: 'Bearer ' + token }, body: '{"name":"Amazon"}' }
+
+        fetchMock
+            .postOnce({
+                matcher: function(url, opts) {
+                    expect(opts).toEqual(param)
+                    return (url === testApiWithAuth.ressourceUrl);
+                },
+                response: {id:1,name:'Amazon'}
+            })
+
+        const store = mockStore()
+
+        return store.dispatch(testApiWithAuth.addResource({name:'Amazon'})).then(() => {
+
+            expect(store.getActions()).toEqual([{ type: 'CREATE_COMPANIES', payload: {id:1, name:'Amazon'} }])
+        })
     })
 })
+
